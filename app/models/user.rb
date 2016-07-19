cec6b6a2bd212a0ea45144cf3951c1b7c4d07e4e
Sandy Grewal <sandy.grewal17@gmail.com>
2016-07-18 23:57:02 -0400

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+	has_secure_password
+
+	validates :email, presence: true
+	validates :firstname, presence: true
+	validates :lastname, presence: true 
+	validates :password, presence: true
+end
